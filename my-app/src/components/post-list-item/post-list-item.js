42e42aad1d23eab297826e3416722761a758a9bf
Nikita Vasiliev <mr.nikitausersan@mail.ru>
2022-09-23 15:50:52 +0300

    some visual bugs were fixed

--- my-app/src/components/post-list-item/post-list-item.js
+++ my-app/src/components/post-list-item/post-list-item.js
@@ -5,9 +5,15 @@ const PostListItem = () => {
         <li className='d-flex justify-content-between border-for spaces'>
             <span className='list-item-data align-items-center '>Hello world</span>
             <div>
-                <button className='btn'>trash</button>
-                <button className='btn'>heart</button>
-                <button className='btn'>star</button>
+                <button className='btn'>
+                    <i className='fa fa-trash'></i>
+                </button>
+                <button className='btn'>
+                    <i className='fa fa-star'></i>
+                </button>
+                <button className='btn'>
+                    <i className='fa fa-heart'></i>
+                </button>
             </div>
         </li>    
     )
--- my-app/src/components/post-list/post-list.js
+++ my-app/src/components/post-list/post-list.js
@@ -3,7 +3,7 @@ import PostListItem from '../post-list-item/post-list-item';
 
 const PostList = () => {
     return (
-        <ul className='post-list justify-content-center'>
+        <ul className='post-list'>
             <PostListItem />
             <PostListItem />
             <PostListItem />
