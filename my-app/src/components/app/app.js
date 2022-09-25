4d12163a11031b3d4d4cf4bd365b6095d31c0529
nikita <mr.nikitausersan@mail.ru>
2022-09-25 22:56:00 +0300

    to check push

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -1,50 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import PostStatusFilter from '../post-status-filter';
 import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
+// hello
+export default class App extends Component {
 
-export default  class App extends Component{
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-      super(props);
+    this.state = {
+      data: [{ label: "fw3nfinfoin3iifw", state: false },
+              { label: "wefwefreferfref", state: true },
+              { label: "ewfreferferferfefrrf", state: false },
+              { label: "sdfsfewqwe wefergfr rtbrybfr r", state: false },
+              { label: "ere err erer erer erre", state: false }]
+    };
 
-      this.state = {
-        data : [{label:"fw3nfinfoin3iifw", state: false},
-                {label:"wefwefreferfref", state: true},
-                {label:"ewfreferferferfefrrf", state: false},
-                {label:"sdfsfewqwe wefergfr rtbrybfr r", state: false},
-                {label:"ere err erer erer erre", state: false}]
-      };
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
 
-      this.addItem = this.addItem.bind(this);
-      this.deleteItem = this.deleteItem.bind(this);
-    }
+  addItem() {
+    console.log("add item");
+  }
 
-    addItem(){
-      console.log("add item");
-    }
+  deleteItem() {
+    console.log("delete item");
+  }
 
-    deleteItem(){
-      console.log("delete item");
-    }
-
-    render() {
-      return (
-        <div className='app border-for'>
-          <AppHeader />
-          <div className='d-flex search-panel'>
-            <SearchPanel />
-            <PostStatusFilter />
-          </div> 
-            <PostList 
-              posts={this.state.data}
-              deleteItem={this.deleteItem}/>
-            <PostAddForm
-              addItem={this.addItem} />
+  render() {
+    return (
+      <div className='app border-for'>
+        <AppHeader />
+        <div className='d-flex search-panel'>
+          <SearchPanel />
+          <PostStatusFilter />
         </div>
-      )
-    }
+        <PostList
+          posts={this.state.data}
+          deleteItem={this.deleteItem} />
+        <PostAddForm
+          addItem={this.addItem} />
+      </div>
+    )
+  }
 }
